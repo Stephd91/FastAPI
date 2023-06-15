119815506679f0e68d178ee0aff9ba5878873c30
StephD91 <stephdumas91@hotmail.com>
2023-06-15 17:31:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1) import FastAPI : a Python class that provides all the functionality for your API.
+from fastapi import FastAPI
+
+# 2) create a FastAPI "instance" : the app variable will be an "instance" of the class FastAPI. 
+# This will be the main point of interaction to create all your API.
+app = FastAPI()
+
+
+
+
+
