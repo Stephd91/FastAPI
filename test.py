4f83e68763925caf58fa6c3ca2fed12f4cb8613a
StephD91 <stephdumas91@hotmail.com>
2023-09-20 17:26:52 +0200

    delete unused file

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "Anki_cards.csv")
-print(csv_file_path)
-
-csv_file = os.path.join("", os.path.curdir, "Anki_cards.csv")
-print(csv_file)
